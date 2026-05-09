55a54f83c5dad78e8a38ebf1db005f377be94357
EvgeniiMal <eugenemal88@gmail.com>
2026-05-09 14:01:03 +0200

    refactor: update paths

--- src/queries/products.ts
+++ src/queries/products.ts
@@ -9,7 +9,7 @@ export function useAvailableProducts() {
     "available-products",
     async () => {
       const res = await axios.get<AvailableProduct[]>(
-        `${API_PATHS.products}/products`
+        API_PATHS.products
       );
       return res.data;
     }
@@ -29,7 +29,7 @@ export function useAvailableProduct(id?: string) {
     ["product", { id }],
     async () => {
       const res = await axios.get<AvailableProduct>(
-        `${API_PATHS.products}/products/${id}`
+        `${API_PATHS.products}/${id}`
       );
       return res.data;
     },
