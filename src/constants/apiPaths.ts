2245d8d74b86376d939286512e3694b43273cd64
EvgeniiMal <eugenemal88@gmail.com>
2026-06-07 20:30:52 +0200

    feat: implement task 8 changes

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: "https://37yguasxvd.execute-api.eu-west-1.amazonaws.com/dev/order",
   import: "https://lwby812yyi.execute-api.eu-west-1.amazonaws.com",
   bff: "https://37yguasxvd.execute-api.eu-west-1.amazonaws.com/dev/bff",
-  cart: "https://37yguasxvd.execute-api.eu-west-1.amazonaws.com/dev/cart",
+  cart: "https://xx09xrrggd.execute-api.eu-west-1.amazonaws.com/api",
 };
 
 export default API_PATHS;
