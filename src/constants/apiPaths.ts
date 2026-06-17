28fdd745144d69cd455a129a0e7e723009f40701
EvgeniiMal <eugenemal88@gmail.com>
2026-06-17 21:15:44 +0200

    refactor(apiPaths):  replace cart api links with beanstalk`s

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 const API_PATHS = {
   base: "https://37yguasxvd.execute-api.eu-west-1.amazonaws.com/dev",
   products: "https://37yguasxvd.execute-api.eu-west-1.amazonaws.com/dev/products",
-  order: "https://xx09xrrggd.execute-api.eu-west-1.amazonaws.com/api/profile/cart",
+  order: "http://evgeniimal-cart-api-develop.eu-west-1.elasticbeanstalk.com/api/profile/cart",
   import: "https://lwby812yyi.execute-api.eu-west-1.amazonaws.com",
   bff: "https://37yguasxvd.execute-api.eu-west-1.amazonaws.com/dev/bff",
-  cart: "https://xx09xrrggd.execute-api.eu-west-1.amazonaws.com/api",
+  cart: "http://evgeniimal-cart-api-develop.eu-west-1.elasticbeanstalk.com/api",
   login: "https://xx09xrrggd.execute-api.eu-west-1.amazonaws.com/api/auth/login",
 };
 
