13bc6523ba921a9fb16b71c9de785da792b78982
EvgeniiMal <eugenemal88@gmail.com>
2026-06-17 22:20:49 +0200

    refactor(apiPaths): replace cart urls with https cloudfront alternative

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 const API_PATHS = {
   base: "https://37yguasxvd.execute-api.eu-west-1.amazonaws.com/dev",
   products: "https://37yguasxvd.execute-api.eu-west-1.amazonaws.com/dev/products",
-  order: "http://evgeniimal-cart-api-develop.eu-west-1.elasticbeanstalk.com/api/profile/cart",
+  order: "https://d2xthvpgpthvif.cloudfront.net/api/profile/cart",
   import: "https://lwby812yyi.execute-api.eu-west-1.amazonaws.com",
   bff: "https://37yguasxvd.execute-api.eu-west-1.amazonaws.com/dev/bff",
-  cart: "http://evgeniimal-cart-api-develop.eu-west-1.elasticbeanstalk.com/api",
+  cart: "https://d2xthvpgpthvif.cloudfront.net/api",
   login: "https://xx09xrrggd.execute-api.eu-west-1.amazonaws.com/api/auth/login",
 };
 
